d81661aa5586faed94fb5044cee01bc9330bc39a
bruceesmith <bruceesmith@gmail.com>
2026-04-28 13:36:29 +1000

    refactor(generator): first phase template simplification in unit test generation

--- generator/tests/header.go
+++ generator/tests/header.go
@@ -20,15 +20,11 @@ func header(name, desc, version string, date string) (builder *strings.Builder, 
 	type data struct {
 		Name    string
 		Date    string
-		Desc    string
 		Version string
 	}
-	first, remainder := desc[0], desc[1:]
-	desc = strings.ToLower(string(first)) + string(remainder)
-	desc = strings.ReplaceAll(desc, "\n", "\n//// ")
 
 	builder = &strings.Builder{}
-	err = headerTmpl.Execute(builder, data{Name: name, Date: date, Desc: desc, Version: version})
+	err = headerTmpl.Execute(builder, data{Name: name, Date: date, Version: version})
 	if err != nil {
 		err = fmt.Errorf("header failed to save result: %w", err)
 	}
--- generator/tests/module.go
+++ generator/tests/module.go
@@ -21,51 +21,51 @@ type GleamModule struct {
 	slots *strings.Builder
 }
 
-func GenerateTests(directory string, modName string, module parser.Module, version string, date string) (err error) {
+func GenerateTests(directory string, module parser.Module, version string, date string) (err error) {
 	gleam := GleamModule{}
-	logger.TraceID("tests", fmt.Sprintf("Module %s: %s\n", modName, module.Description))
+	logger.TraceID("tests", fmt.Sprintf("Module %s", module.Name))
 
-	fileName := strcase.ToSnake(modName)
+	fileName := strcase.ToSnake(module.Name)
 	file, err := os.Create(filepath.Join(directory, fileName+"_test.gleam"))
 	if err != nil {
 		return fmt.Errorf("failed to create file %s: %w", fileName+".gleam", err)
 	}
 	defer file.Close()
 
-	gleam.header, err = header(modName, module.Description, version, date)
+	gleam.header, err = header(module.Name, module.Description, version, date)
 	if err != nil {
-		return fmt.Errorf("processing of %s failed: %w", modName, err)
+		return fmt.Errorf("processing of %s failed: %w", module.Name, err)
 	}
 
-	gleam.imports, err = imports(module, modName)
+	gleam.imports, err = imports(module, module.Name)
 	if err != nil {
-		return fmt.Errorf("processing of %s failed: %w", modName, err)
+		return fmt.Errorf("processing of %s failed: %w", module.Name, err)
 	}
 	// enumerations = append(enumerations, definers...)
 
 	gleam.configuration, err = config(module)
 	if err != nil {
-		return fmt.Errorf("processing of %s failed: %w", modName, err)
+		return fmt.Errorf("processing of %s failed: %w", module.Name, err)
 	}
 
 	// gleam.constructor, err = constructors(modName, module.Attributes)
 	// if err != nil {
-	// 	return fmt.Errorf("processing of %s failed: %w", modName, err)
+	// 	return fmt.Errorf("processing of %s failed: %w", module.Name, err)
 	// }
 
 	// gleam.setters, err = setters(modName, module.Attributes)
 	// if err != nil {
-	// 	return fmt.Errorf("processing of %s failed: %w", modName, err)
+	// 	return fmt.Errorf("processing of %s failed: %w", module.Name, err)
 	// }
 
 	// gleam.renderers, err = render(modName, module.Tag, module.Attributes)
 	// if err != nil {
-	// 	return fmt.Errorf("processing of %s failed: %w", modName, err)
+	// 	return fmt.Errorf("processing of %s failed: %w", module.Name, err)
 	// }
 
-	gleam.slots, err = slots(module.Slots, modName)
+	gleam.slots, err = slots(module.Slots, module.Name)
 	if err != nil {
-		return fmt.Errorf("processing of %s failed: %w", modName, err)
+		return fmt.Errorf("processing of %s failed: %w", module.Name, err)
 	}
 
 	write(file, gleam)
